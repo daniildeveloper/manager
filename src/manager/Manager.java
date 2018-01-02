85ebd2460d14298457439b8268e67c7cddd90815
Daniil Borovkov <daniilborowkow@yandex.ru>
2018-01-02 18:01:01 +0600

    Removed useless code for javafx. Migrated to maven&eclipse

--- src/manager/Manager.java
+++ src/manager/Manager.java
@@ -2,35 +2,34 @@
  */
 package manager;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /**
  *
- * @author daniiltserin
+ * @author daniildeveloper
  */
 public class Manager extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction((ActionEvent event) -> {
-            System.out.println("Hello World!");
-        });
+        try {
+            AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("mainwindow/MainWindow.fxml"));
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+            Scene scene = new Scene(root, 1280, 700);
 
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+            primaryStage.setTitle("Hello World!");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(Manager.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
---
+++ src/manager/mainwindow/MainWindowController.java
@@ -0,0 +1,29 @@
+/*
+ */
+package manager.mainwindow;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author daniildeveloper
+ */
+public class MainWindowController implements Initializable {
+
+//    @FXML
+//    private Tab tabMail;
+    /**
+     * Initializes the controller class.
+     *
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+//        tabMail.setGraphic(new Label("Mail"));
+    }
+
+}
